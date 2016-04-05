2455e4d8f11b87814ac7721e0c3258fa7cde7920
Gaffel15 <thegaffel15@gmail.com>
2016-04-05 14:04:21 +0200

    stuff

--- FBXReader/MaterialHandler.h
+++ FBXReader/MaterialHandler.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef MATERIALHANDLER_H
+#define MATERIALHANDLER_H
+
+#include "fbxSDK.h"
+
+
+class MaterialHandler
+{
+public:
+
+
+private:
+
+};
+
+
+#endif // !MATERIALHANDLER_H
