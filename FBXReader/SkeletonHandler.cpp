f3399de1e1b05c4151f31b70b6d6ca7f6141482d
David Wigelius <david.wigelius@gmail.com>
2016-04-06 16:57:01 +0200

    Made things
    Made a new fbx with a skeleton and some simple code to test if the
    skeleton even enters the function

--- FBXReader/SkeletonHandler.cpp
+++ FBXReader/SkeletonHandler.cpp
@@ -10,3 +10,18 @@ SkeletonHandler::SkeletonHandler()
 SkeletonHandler::~SkeletonHandler()
 {
 }
+
+void SkeletonHandler::GetSkeletonData(FbxNode * pNode)
+{
+	if (pNode->GetSkeleton())
+	{
+		std::cout << "skeleton name: " << pNode->GetName() << "\n";
+		ProcessData(pNode->GetSkeleton());
+	}
+}
+
+void SkeletonHandler::ProcessData(FbxSkeleton * pSkeleton)
+{
+	//pSkeleton->IsSkeletonRoot
+	std::cout << (char*)pSkeleton->GetSkeletonType();
+}
