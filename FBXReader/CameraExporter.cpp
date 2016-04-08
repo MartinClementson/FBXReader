b9fdbcff2d177f400e56541b109ddbaa13276ba8
Gaffel15 <thegaffel15@gmail.com>
2016-04-08 11:25:42 +0200

    added couts

--- FBXReader/CameraExporter.cpp
+++ FBXReader/CameraExporter.cpp
@@ -6,3 +6,18 @@ CameraExporter::CameraExporter()
 {
 	cameras = new std::vector<CameraHeader>;
 }
+
+CameraExporter::~CameraExporter()
+{
+	delete cameras;
+}
+
+
+void CameraExporter::WriteToBinaryFile(std::ofstream* outfile)
+{
+	std::cout << "Intrest position: X:" << cameras->at(0).intrest[0] << "Y: " << cameras->at(0).intrest[1] << "Y: " << cameras->at(0).intrest[2] << "\n";
+
+	std::cout << "Camera position: X:" << cameras->at(0).position[0] << "Y: " << cameras->at(0).position[1] << "Y: " << cameras->at(0).position[2] << "\n";
+
+	std::cout << "Camera Roll" << cameras->at(0).roll;
+}
