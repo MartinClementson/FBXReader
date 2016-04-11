1c6da8f5e4c401a20ff9172293a8bb3f33760e29
David Wigelius <david.wigelius@gmail.com>
2016-04-11 13:44:38 +0200

    made declarations
    made declarations for the class

--- FBXReader/SkeletonExport.h
+++ FBXReader/SkeletonExport.h
@@ -1,8 +1,17 @@
 #pragma once
+#include "BRFImporterStructs.h"
+#include "fbxSDK.h"
+using namespace BRFImporter;
+
 class SkeletonExport
 {
 public:
 	SkeletonExport();
 	~SkeletonExport();
+	SkeletonHeader skeletonInfo;
+	std::vector<JointHeader>* joints;
+	std::vector<AnimationHeader>* animations;
+	std::vector<JointCountHeader>* animationJointCount;
+	std::vector<FrameHeader>* frames;
 };
 
