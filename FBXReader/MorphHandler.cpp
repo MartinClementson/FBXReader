5221bd83bfe6a7a765ac0914f910753ebb4bc139
Gaffel15 <thegaffel15@gmail.com>
2016-04-11 11:11:22 +0200

    done wif blend

--- FBXReader/MorphHandler.cpp
+++ FBXReader/MorphHandler.cpp
@@ -23,41 +23,49 @@ void MorphHandler::GetMorphData(FbxNode* pNode)
 	int targetShapeCount;
 
 	std::cout << "blendCount!!!!! " << morphAnimCount << "\n\n\n\n";
-	FbxBlendShape* morphAnim;
-	morphAnim = (FbxBlendShape*)pGeo->GetDeformer(0, FbxDeformer::eBlendShape);
-
-	morphChannelCount = morphAnim->GetBlendShapeChannelCount();
-	std::cout << "ChannelCount!!!!! " << morphChannelCount << "\n\n\n\n";
-	for (unsigned int i = 0; i < morphChannelCount; i++)
+	if (morphAnimCount > 0)
 	{
-		std::cout << "channel nr: " << i << "\n";
-
-		FbxBlendShapeChannel* morphChannel;
-		morphChannel = morphAnim->GetBlendShapeChannel(i);
-
-		std::cout << "ChannelName!!!!! " << morphChannel->GetName() << "\n\n\n\n";
-
-		targetShapeCount = morphChannel->GetTargetShapeCount();
-
-		std::cout << "Target Shape Count!!!!! " << targetShapeCount << "\n\n\n\n";
-		for (unsigned int i = 0; i < targetShapeCount; i++)
+		for (unsigned int i = 0; i < morphAnimCount; i++)
 		{
-			FbxShape* shape;
-			shape = morphChannel->GetTargetShape(i);
-			
-			std::cout << "Shape name!!!!! " << shape->GetName() << "\n\n\n\n";
+			FbxBlendShape* morphAnim;
+			morphAnim = (FbxBlendShape*)pGeo->GetDeformer(i, FbxDeformer::eBlendShape);
 
+			morphChannelCount = morphAnim->GetBlendShapeChannelCount();
+			std::cout << "ChannelCount!!!!! " << morphChannelCount << "\n\n\n\n";
 
-			int vertexCount = shape->GetControlPointsCount();
-			FbxVector4* vertices = shape->GetControlPoints();
-
-			for (int i = 0; i < vertexCount; i++)
+			for (unsigned int j = 0; j < morphChannelCount; j++)
 			{
-				std::cout << " x? " << ((double*)shape->GetControlPointAt(i))[0] << "";
-				std::cout << " y? " << ((double*)shape->GetControlPointAt(i))[1] << "";
-				std::cout << " z? " << ((double*)shape->GetControlPointAt(i))[2] << "\n\n";
+				std::cout << "channel nr: " << j << "\n";
+
+				FbxBlendShapeChannel* morphChannel;
+				morphChannel = morphAnim->GetBlendShapeChannel(j);
+
+				std::cout << "ChannelName!!!!! " << morphChannel->GetName() << "\n\n\n\n";
+
+				targetShapeCount = morphChannel->GetTargetShapeCount();
+
+				std::cout << "Target Shape Count!!!!! " << targetShapeCount << "\n\n\n\n";
+				for (unsigned int k = 0; k < targetShapeCount; k++)
+				{
+					FbxShape* shape;
+					shape = morphChannel->GetTargetShape(k);
+
+					std::cout << "Shape name!!!!! " << shape->GetName() << "\n\n\n\n";
+
+
+					int vertexCount = shape->GetControlPointsCount();
+					FbxVector4* vertices = shape->GetControlPoints();
+
+					for (int l = 0; l < vertexCount; l++)
+					{
+						std::cout << " x? " << ((double*)shape->GetControlPointAt(l))[0] << "";
+						std::cout << " y? " << ((double*)shape->GetControlPointAt(l))[1] << "";
+						std::cout << " z? " << ((double*)shape->GetControlPointAt(l))[2] << "\n\n";
+					}
+				}
 			}
 		}
+
 	}
 }
 
