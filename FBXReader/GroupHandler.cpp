0168c9e4098226c6138cb1aee126a6f5c3bd707f
surethang94 <surethang@live.com>
2016-04-13 12:52:31 +0200

    groups complete

--- FBXReader/GroupHandler.cpp
+++ FBXReader/GroupHandler.cpp
@@ -9,7 +9,6 @@ GroupHandler::GroupHandler()
 
 GroupHandler::~GroupHandler()
 {
-
 }
 
 void GroupHandler::GetGroupData(FbxNode * pNode, std::vector<GroupExport*>*outputGroup)
@@ -19,30 +18,13 @@ void GroupHandler::GetGroupData(FbxNode * pNode, std::vector<GroupExport*>*outpu
 
 	if (lAttributeType == FbxNodeAttribute::EType::eNull)
 	{
-		GroupExport* tmpGroup = new GroupExport();
-		std::cout << pNode->GetName() << std::endl;
+		//GroupExport* tmpGroup = new GroupExport();
 
-		memcpy(tmpGroup->groupInfo.groupName, pNode->GetName(), sizeof(char) * 256);
-
-		FbxDouble3 translation = pNode->LclTranslation.Get();
-		tmpGroup->groupInfo.translation[0] = translation[0];
-		tmpGroup->groupInfo.translation[1] = translation[1];
-		tmpGroup->groupInfo.translation[2] = translation[2];
-
-		FbxDouble3 rotation = pNode->LclRotation.Get();
-		tmpGroup->groupInfo.rotation[0] = rotation[0];
-		tmpGroup->groupInfo.rotation[1] = rotation[1];
-		tmpGroup->groupInfo.rotation[2] = rotation[2];
-
-		FbxDouble3 scaling = pNode->LclScaling.Get();
-		tmpGroup->groupInfo.scale[0] = scaling[0];
-		tmpGroup->groupInfo.scale[1] = scaling[1];
-		tmpGroup->groupInfo.scale[2] = scaling[2];
-
-		
 		int attrCount = pNode->GetNodeAttributeCount();
 		FbxProperty prop = pNode->GetFirstProperty();
 		std::vector<FbxProperty> properties;
+
+		int i = 0;
 		while (prop != NULL)
 		{
 			if( prop.GetName().Find("Attr_") == 0 || prop.GetName().Find("attr_") == 0)
@@ -54,8 +36,11 @@ void GroupHandler::GetGroupData(FbxNode * pNode, std::vector<GroupExport*>*outpu
 			
 		}
 
+
 			for (int i = 0; i < properties.size(); i++)
 			{
+				GroupExport* tmpGroup = new GroupExport();
+
 				std::cout << "\n\nGroupAttr: " << properties[i].GetName() << "\n\n";
 				if (properties[i].HasMaxLimit())
 				{
@@ -69,43 +54,81 @@ void GroupHandler::GetGroupData(FbxNode * pNode, std::vector<GroupExport*>*outpu
 				{
 				case eFbxBool:
 					std::cout << "\n\nDataType:  " << properties[i].GetPropertyDataType().GetName() << std::endl;
+		
+
 					std::cout << "\n\nTrue/false: " << properties[i].Get<FbxBool>() << std::endl;
+
 					break;
 				case eFbxFloat:
 					std::cout << "\n\nDataType:  " << properties[i].GetPropertyDataType().GetName() << std::endl;
+					
+
 					std::cout << "\n\nFloat: " << properties[i].Get<FbxFloat>()<< std::endl;
 					break;
 				case eFbxString:
 					std::cout << "\n\nDataType:  " << properties[i].GetPropertyDataType().GetName() << std::endl;
+					
+
 					std::cout << "\n\nString: " << properties[i].Get<FbxString>()<< std::endl;
 					break;
 				case eFbxInt:
 					std::cout << "\n\nDataType:  " << properties[i].GetPropertyDataType().GetName() << std::endl;
+
 					std::cout << "\n\nInt: " << properties[i].Get<FbxInt>()<< std::endl;
 					break;
 				case eFbxEnum:
 					std::cout << "\n\nDataType:  " << properties[i].GetPropertyDataType().GetName() << std::endl;
+
 					std::cout << "\n\n enum: " <<properties[i].Get<FbxEnum>()<< std::endl;
 					break;
 				case eFbxDouble:
 					std::cout << "\n\nDataType:  " << properties[i].GetPropertyDataType().GetName() << std::endl;
 					std::cout << "\n\nDouble: " <<properties[i].Get<FbxDouble>()<< std::endl;
 					break;
-				}
+				}//switch end
 
 				FbxInt attrcount = properties.size();
+
+
+				FbxString attrname = properties[i].GetName();
+	
+
+				FbxString datatype = properties[i].GetPropertyDataType().GetName();
 				tmpGroup->groupInfo.attrCount = attrcount;
 
-				memcpy(tmpGroup->groupInfo.groupAttrName, properties[i].GetName(), sizeof(char) * 256);
+				//tmpGroup->groupInfo.dataType = properties[i].GetPropertyDataType().GetName();
 
-				memcpy(tmpGroup->groupInfo.dataType, properties[i].GetPropertyDataType().GetName(), sizeof(char) * 256);
+				memcpy(tmpGroup->groupInfo.groupAttrName,attrname, sizeof(char)*256);
 
-				
+				memcpy(tmpGroup->groupInfo.dataType, datatype, sizeof(char)*256);
+
+				std::cout << pNode->GetName() << std::endl;
+
+				memcpy(tmpGroup->groupInfo.groupName, pNode->GetName(), sizeof(char) * 256);
+
+				FbxDouble3 translation = pNode->LclTranslation.Get();
+				tmpGroup->groupInfo.translation[0] = translation[0];
+				tmpGroup->groupInfo.translation[1] = translation[1];
+				tmpGroup->groupInfo.translation[2] = translation[2];
+
+				FbxDouble3 rotation = pNode->LclRotation.Get();
+				tmpGroup->groupInfo.rotation[0] = rotation[0];
+				tmpGroup->groupInfo.rotation[1] = rotation[1];
+				tmpGroup->groupInfo.rotation[2] = rotation[2];
+
+				FbxDouble3 scaling = pNode->LclScaling.Get();
+				tmpGroup->groupInfo.scale[0] = scaling[0];
+				tmpGroup->groupInfo.scale[1] = scaling[1];
+				tmpGroup->groupInfo.scale[2] = scaling[2];
+
+
 				outputGroup->push_back(tmpGroup);
 
-			}
+				
 
-		outputGroup->push_back(tmpGroup);
+			}//for end
+
+		//outputGroup->push_back(tmpGroup);
 	}
 
 
